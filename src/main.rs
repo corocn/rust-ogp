6bb96600a9f0e1dea992a78d9a22124a2da3d86a
corocn <crsk.plmzh@gmail.com>
2021-08-19 10:55:51 +0900

    整理

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-// use rusttype::{Font, Scale, point};
-use image::{Rgba, GenericImageView, GenericImage, DynamicImage};
-use imageproc::drawing::{draw_text_mut, Canvas};
-
-// use fontdue::layout::{CoordinateSystem, Layout, LayoutSettings, TextStyle};
-// use fontdue::{Font, FontSettings};
-
+use image::Rgba;
+use imageproc::drawing::draw_text_mut;
 use rusttype::{Font, Scale};
 
 fn main() {
     let mut image = image::open("assets/image/bg1.jpg").unwrap();
-
-    // println!("dimensions {:?}", image.dimensions());
-    // println!("{:?}", image.color());
-
-    // image.save("tmp/test.png").unwrap();
-    //
-    // let thumb = image.thumbnail(200, 200);
-    // thumb.save("tmp/thumb.jpg").unwrap();
-
-    // let font = Vec::from(include_bytes!("../assets/fonts/Roboto-Regular.ttf") as &[u8]);
     let font = Vec::from(include_bytes!("../assets/fonts/DelaGothicOne-Regular.ttf") as &[u8]);
     let font = Font::try_from_vec(font).unwrap();
 
